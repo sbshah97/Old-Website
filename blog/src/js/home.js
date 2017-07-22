0be874b0344d9843ede58c65548ea2f9d35ff78a
salman-bhai <sbs.191197@gmail.com>
2017-07-22 18:34:21 +0530

    Gemfile settings changed

--- blog/src/js/home.js
+++
@@ -1,8 +0,0 @@
-$( document ).ready(function(){
-	$("#sidenav").sideNav();
-	$(document).scroll(function() { 
-		var $nav = $('.navbar-fixed');
-		$nav.find('nav').toggleClass('orange', $(this).scrollTop() > $('.home-page-banner').height());
-		$nav.find('nav').toggleClass('transparent', $(this).scrollTop() < $('.home-page-banner').height());
-    });
-});
